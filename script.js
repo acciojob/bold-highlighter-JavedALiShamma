544fc28fbd5f99f586dea392b15d424f4ebd6453
JavedALiShamma <153979997+JavedALiShamma@users.noreply.github.com>
2024-02-01 20:46:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-function highlight() {
+function highlight(e) {
     //Write your code here
-
+ // Here we have the question to highlight the all the bold events
+//  Here we are already in the function
+	let strongEle=document.getElementsByTagName("strong");
+	console.log(strongEle);
+	for(let i=0;i<strongEle.length;i++){
+		// strongEle[i].classList.toggle("greenColor");
+		strongEle[i].style.color="green";
+	}
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+let strongEle=document.getElementsByTagName("strong");
+	console.log(strongEle);
+	for(let i=0;i<strongEle.length;i++){
+		// strongEle[i].classList.toggle("greenColor");
+		strongEle[i].style.color="black";
+	}
     
 }
